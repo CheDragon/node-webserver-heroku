26d115707a9b19e25fc1aabe005c2172ef5240f5
Rene Trujillo <chedragon18@gmail.com>
2022-03-12 21:35:18 +0200

    Despliegue app con angular

--- app.js
+++ app.js
@@ -13,29 +13,29 @@ hbs.registerPartials(__dirname + '/views/partials', function (err) { console.log
 // Servir contenido estático
 app.use( express.static('public') );
 
-app.get('/', function (req, res) {
-    res.render('home', {
-        nombre: 'Rene Trujillo',
-        titulo: 'Aprendiendo Node'
-    });
-});
+// app.get('/', function (req, res) {
+//     res.render('home', {
+//         nombre: 'Rene Trujillo',
+//         titulo: 'Aprendiendo Node'
+//     });
+// });
 
-app.get('/generic', function (req, res) {
-    res.render('generic', {
-        nombre: 'Rene Trujillo',
-        titulo: 'Aprendiendo Node'
-    });
-});
+// app.get('/generic', function (req, res) {
+//     res.render('generic', {
+//         nombre: 'Rene Trujillo',
+//         titulo: 'Aprendiendo Node'
+//     });
+// });
 
-app.get('/elements', function (req, res) {
-    res.render('elements', {
-        nombre: 'Rene Trujillo',
-        titulo: 'Aprendiendo Node'
-    });
-});
+// app.get('/elements', function (req, res) {
+//     res.render('elements', {
+//         nombre: 'Rene Trujillo',
+//         titulo: 'Aprendiendo Node'
+//     });
+// });
 
 app.get('*', function (req, res) {
-    res.sendFile( __dirname + '/public/404.html');
+    res.sendFile( __dirname + '/public/index.html');
 });
 
 app.listen(port, () => {
